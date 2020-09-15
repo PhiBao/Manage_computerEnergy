7dd3ede9060fe4121780c7d83ac61492aef6c6a7
Kiter <kiter0211@gmail.com>
2020-09-15 22:01:32 +0700

    Overall GUI

---
+++ src/application/Controller.java
@@ -0,0 +1,46 @@
+package application;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+	@FXML
+	private Label dateTimeLabel;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		setDateAndTime();
+
+	}
+
+	private void setDateAndTime() {
+		Thread timerThread = new Thread(() -> {
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+
+			while (true) {
+				try {
+					Thread.sleep(1000); // 1 second
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+				}
+				final String time = simpleDateFormat.format(new Date());
+				Platform.runLater(() -> {
+					dateTimeLabel.setText(time);
+
+				});
+			}
+		});
+		// Start the thread
+		timerThread.start();
+	}
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,29 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {	
+		try {
+			Parent root = FXMLLoader.load(this.getClass().getResource("GUI.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("System Manage Computer Energy");
+			primaryStage.show();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+			
+		}
+	}
+}
