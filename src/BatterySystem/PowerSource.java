76fa78452a52f66f4fb7ef137217417e3a86d104
Kiter <kiter0211@gmail.com>
2020-09-16 22:24:01 +0700

    Display battery system

---
+++ src/BatterySystem/PowerSource.java
@@ -0,0 +1,63 @@
+package BatterySystem;
+
+import com.sun.jna.NativeLong;
+
+import BatterySystem.PowerProfile.SystemBatteryState;
+
+public class PowerSource {
+
+	private String name;
+	private double remainingCapacity;
+	private double timeRemaining;
+
+	public PowerSource(String name, double remainingCapacity, double timeRemaining) {
+		this.name = name;
+		this.remainingCapacity = remainingCapacity;
+		this.timeRemaining = timeRemaining;
+		
+	}
+
+	public String getName() {
+		return this.name;
+		
+	}
+
+	public double getRemainingCapacity() {
+		return this.remainingCapacity;
+		
+	}
+
+	public double getTimeRemaining() {
+		return this.timeRemaining;
+		
+	}
+
+	public static PowerSource[] getPowerSources() {
+		String name = "System Battery";
+		PowerSource[] psArray = new PowerSource[1];
+		// Get structure
+		SystemBatteryState batteryState = new SystemBatteryState();
+		if (0 != PowerProfile.INSTANCE.CallNtPowerInformation(PowerProfile.SYSTEM_BATTERY_STATE, null,
+				new NativeLong(0), batteryState, new NativeLong(batteryState.size()))
+				|| batteryState.batteryPresent == 0) {
+			psArray[0] = new PowerSource("Unknown", 0d, -1d);
+		} else {
+			int estimatedTime = -2; // -1 = unknown, -2 = unlimited
+			if (batteryState.acOnLine == 0 && batteryState.charging == 0 && batteryState.discharging > 0) {
+				estimatedTime = batteryState.estimatedTime;
+			}
+			long maxCapacity = getUnsignedInt(batteryState.maxCapacity);
+			long remainingCapacity = getUnsignedInt(batteryState.remainingCapacity);
+
+			psArray[0] = new PowerSource(name, (double) remainingCapacity / maxCapacity, estimatedTime);
+		}
+
+		return psArray;
+	}
+
+	public static long getUnsignedInt(int x) {
+		return x & 0x00000000ffffffffL;
+		
+	}
+
+}
--- src/application/Controller.java
+++ src/application/Controller.java
@@ -9,14 +9,66 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.ResourceBundle;
 
+import BatterySystem.PowerSource;
+
 public class Controller implements Initializable {
 
 	@FXML
 	private Label dateTimeLabel;
 
+	@FXML
+	private Label powerLabel;
+
+	@FXML
+	private Label batteryLabel;
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		setDateAndTime();
+		setBatterySystem();
+
+	}
+
+	private void setBatterySystem() {
+		// TODO Auto-generated method stub
+		Thread batteryThread = new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(10000); // 10s
+
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+
+				}
+				StringBuilder sb = new StringBuilder("Power: ");
+				if (PowerSource.getPowerSources().length == 0) {
+					sb.append("Unknown");
+				} else {
+					double timeRemaining = PowerSource.getPowerSources()[0].getTimeRemaining();
+					if (timeRemaining < -1d)
+						sb.append("Charging");
+					else if (timeRemaining < 0d)
+						sb.append("Calculating time remaining");
+					else
+						sb.append(String.format("%d:%02d remaining", (int) (timeRemaining / 3600),
+								(int) (timeRemaining / 60) % 60));
+				}
+				for (PowerSource pSource : PowerSource.getPowerSources()) {
+					sb.append(
+							String.format("%n %s @ %.1f%%", pSource.getName(), pSource.getRemainingCapacity() * 100d));
+				}
+				final String tmp[] = sb.toString().split("\r");
+				
+				Platform.runLater(() -> {
+					powerLabel.setText(tmp[0].trim());
+					batteryLabel.setText(tmp[1].trim());
+
+				});
+				
+			}
+		});
+		// Start the thread
+		batteryThread.start();
 
 	}
 
