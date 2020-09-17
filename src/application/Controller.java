bc795eff94a03bd0679d345f81f09fdae66781fe
Kiter <kiter0211@gmail.com>
2020-09-17 11:06:25 +0700

    improve Thread code

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -30,69 +30,80 @@ public class Controller implements Initializable {
 	}
 
 	private void setBatterySystem() {
-		// TODO Auto-generated method stub
-		Thread batteryThread = new Thread(() -> {
-			while (true) {
-				try {
-					Thread.sleep(10000); // 10s
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					StringBuilder sb = new StringBuilder("Power: ");
+					
+					if (PowerSource.getPowerSources().length == 0) {
+						sb.append("Unknown");
+					} else {
+						double timeRemaining = PowerSource.getPowerSources()[0].getTimeRemaining();
+						if (timeRemaining < -1d)
+							sb.append("Charging");
+						else if (timeRemaining < 0d)
+							sb.append("Calculating time remaining");
+						else
+							sb.append(String.format("%d:%02d remaining", (int) (timeRemaining / 3600),
+									(int) (timeRemaining / 60) % 60));
+					}
+					
+					for (PowerSource pSource : PowerSource.getPowerSources()) {
+						sb.append(String.format("%n %s @ %.1f%%", pSource.getName(),
+								pSource.getRemainingCapacity() * 100d));
+					}
+					final String tmp[] = sb.toString().split("\r");
 
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							powerLabel.setText(tmp[0].trim());
+							batteryLabel.setText(tmp[1].trim());
 
-				}
-				StringBuilder sb = new StringBuilder("Power: ");
-				if (PowerSource.getPowerSources().length == 0) {
-					sb.append("Unknown");
-				} else {
-					double timeRemaining = PowerSource.getPowerSources()[0].getTimeRemaining();
-					if (timeRemaining < -1d)
-						sb.append("Charging");
-					else if (timeRemaining < 0d)
-						sb.append("Calculating time remaining");
-					else
-						sb.append(String.format("%d:%02d remaining", (int) (timeRemaining / 3600),
-								(int) (timeRemaining / 60) % 60));
-				}
-				for (PowerSource pSource : PowerSource.getPowerSources()) {
-					sb.append(
-							String.format("%n %s @ %.1f%%", pSource.getName(), pSource.getRemainingCapacity() * 100d));
-				}
-				final String tmp[] = sb.toString().split("\r");
-				
-				Platform.runLater(() -> {
-					powerLabel.setText(tmp[0].trim());
-					batteryLabel.setText(tmp[1].trim());
+						}
 
-				});
-				
+					});
+
+					try {
+						Thread.sleep(30000); // 30s
+
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+
+					}
+				}
 			}
-		});
-		// Start the thread
-		batteryThread.start();
-
+		}).start();
 	}
 
 	private void setDateAndTime() {
-		Thread timerThread = new Thread(() -> {
-			SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE | dd/MM/yyyy HH:mm:ss");
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE | dd/MM/yyyy HH:mm:ss");
 
-			while (true) {
-				try {
-					Thread.sleep(1000); // 1 second
+				while (true) {
+					final String time = simpleDateFormat.format(new Date());
 
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+					Platform.runLater(new Runnable() {
+						@Override
+						public void run() {
+							dateTimeLabel.setText(time);
+						}
 
+					});
+
+					try {
+						Thread.sleep(1000); // 1 second
+
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+
+					}
 				}
-				final String time = simpleDateFormat.format(new Date());
-				Platform.runLater(() -> {
-					dateTimeLabel.setText(time);
-
-				});
 			}
-		});
-		// Start the thread
-		timerThread.start();
+		}).start();
 	}
 
 }
--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -13,17 +16,25 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(Stage primaryStage) {	
+	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(this.getClass().getResource("GUI.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("System Manage Computer Energy");
 			primaryStage.show();
-			
+
 		} catch (Exception e) {
 			System.out.println(e);
-			
+
 		}
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent e) {
+				Platform.exit();
+				System.exit(0);
+			}
+		});
 	}
 }
